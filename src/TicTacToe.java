296955b725c696d6386ebec82dbce7be160c22b6
Praghya <pg6408@srmist.edu.in>
2026-04-21 10:47:35 +0530

    UC9: Added win condition check (rows, columns, diagonals)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,28 +1,125 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
+
+    static char humanSymbol = 'X';
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+
+        while (!gameOver) {
+
+            if (isHumanTurn) {
+                System.out.println("Human move:");
+                // sample move (you can later take input)
+                placeMove(0, 0, humanSymbol);
+            } else {
+                System.out.println("Computer move:");
+                computerMove();
+            }
+
+            printBoard();
+
+            // ✅ UC9: Check win
+            if (hasWon(humanSymbol)) {
+                System.out.println("Human wins!");
+                gameOver = true;
+            } else if (hasWon(computerSymbol)) {
+                System.out.println("Computer wins!");
+                gameOver = true;
+            } else if (isBoardFull()) {
+                System.out.println("It's a draw!");
+                gameOver = true;
+            }
+
+            isHumanTurn = !isHumanTurn;
+        }
     }
 
-    static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+    // UC6
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    // UC7
+    static void computerMove() {
+        Random rand = new Random();
+
+        while (true) {
+            int slot = rand.nextInt(9) + 1;
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == '-') {
+                placeMove(row, col, computerSymbol);
+                break;
             }
         }
     }
 
-    static void printBoard() {
-        System.out.println("Current Board:");
+    // UC9 ⭐ MAIN LOGIC
+    static boolean hasWon(char symbol) {
 
+        // rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol &&
+                    board[i][1] == symbol &&
+                    board[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        // columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol &&
+                    board[1][j] == symbol &&
+                    board[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        // diagonals
+        if (board[0][0] == symbol &&
+                board[1][1] == symbol &&
+                board[2][2] == symbol) {
+            return true;
+        }
+
+        if (board[0][2] == symbol &&
+                board[1][1] == symbol &&
+                board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') return false;
+            }
+        }
+        return true;
+    }
+
+    static void printBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
